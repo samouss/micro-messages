111124fec0796ff8e18f2f013ce2e79194b5e330
Vaillant Samuel <samuel.vllnt@gmail.com>
2017-06-06 15:57:21 +0200

    feat(fixtures): sort messages

--- scripts/fixtures.js
+++ scripts/fixtures.js
@@ -2,16 +2,19 @@ const faker = require('faker');
 const uuid = require('uuid');
 const fs = require('fs');
 const path = require('path');
+const { orderBy } = require('lodash');
 
-const sample = new Array(20).fill(null).map(() => ({
+const messages = new Array(20).fill(null).map(() => ({
   id: uuid.v4(),
   date: faker.date.past(),
   visibility: ['public', 'private'][Math.floor(Math.random() * 2)],
   body: faker.lorem.paragraph(),
 }));
 
+const messagesOrderByDate = orderBy(messages, x => x.date, ['desc']);
+
 fs.writeFileSync(
   path.join(__dirname, '..', 'db.json'),
   // eslint-disable-next-line
-  JSON.stringify(sample, null, 2)
+  JSON.stringify(messagesOrderByDate, null, 2)
 );
