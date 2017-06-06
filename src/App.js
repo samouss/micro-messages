04af5d513e58c8cd86a0f16ec0711a1c1fc5bb01
Vaillant Samuel <samuel.vllnt@gmail.com>
2017-06-06 21:51:11 +0200

    feat(App): tie components

--- src/App.js
+++ src/App.js
@@ -3,27 +3,25 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import configureStore from './store/configureStore';
+import configureStore from 'store/configureStore';
+import Layout from 'components/Layout';
+import Sidebar from 'components/Sidebar';
+import VisibleMessagesList from 'modules/messages/containers/VisibleMessagesList';
+import VisibleMessage from 'modules/messages/containers/VisibleMessage';
 import './App.css';
-
-const List = () => (
-  <div>list messages</div>
-);
-
-const Message = () => (
-  <div>view message</div>
-);
 
 const store = configureStore();
 
 const App = () => (
   <Provider store={store}>
     <Router>
-      <div>
-        <List />
+      <Layout>
+        <Sidebar>
+          <Route component={VisibleMessagesList} />
+        </Sidebar>
 
-        <Route path="/:messageId" component={Message} />
-      </div>
+        <Route path="/:messageId" component={VisibleMessage} />
+      </Layout>
     </Router>
   </Provider>
 );
