16bc9a1c0f92b2d236a4ad348c87ee09cf5e57ab
Vaillant Samuel <samuel.vllnt@gmail.com>
2017-06-06 18:07:19 +0200

    core(fixtures): use paragraphs

--- scripts/fixtures.js
+++ scripts/fixtures.js
@@ -8,7 +8,7 @@ const messages = new Array(20).fill(null).map(() => ({
   id: uuid.v4(),
   date: faker.date.past(),
   visibility: ['public', 'private'][Math.floor(Math.random() * 2)],
-  body: faker.lorem.paragraph(),
+  body: faker.lorem.paragraphs(),
 }));
 
 const messagesOrderByDate = orderBy(messages, x => x.date, ['desc']);
