b26eef447bd7c90833c22c91a92baa6091e55b5a
Vaillant Samuel <samuel.vllnt@gmail.com>
2017-06-08 11:14:17 +0200

    feat(messages): improve test for VisibleMessage

--- src/modules/messages/containers/__tests__/VisibleMessage.js
+++ src/modules/messages/containers/__tests__/VisibleMessage.js
@@ -3,11 +3,21 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { createMockMessage } from 'test/messages';
-import { VisibleMessage } from '../VisibleMessage';
+import { createMockStore } from 'test/store';
+import { getMessage } from '../../reducers';
+import Connect, { VisibleMessage } from '../VisibleMessage';
 
 import type { Props } from '../VisibleMessage';
 
+jest.mock('../../reducers', () => ({
+  getMessage: jest.fn(state => state),
+}));
+
 describe('<VisibleMessage />', () => {
+  const createContext = state => ({
+    store: createMockStore(state),
+  });
+
   const defaultProps = {
     message: createMockMessage('ID1234'),
   };
@@ -39,5 +49,28 @@ describe('<VisibleMessage />', () => {
     );
 
     expect(component).toMatchSnapshot();
+  });
+
+  describe('mapStateToProps', () => {
+    it('expect to call getMessage', () => {
+      const state = { messages: [] };
+      const context = createContext(state);
+
+      const messageId = 'ID1234';
+      const props = {
+        match: {
+          params: { messageId },
+        },
+      };
+
+      shallow(
+        <Connect
+          {...props}
+        />,
+        { context },
+      );
+
+      expect(getMessage).toHaveBeenCalledWith(state, messageId);
+    });
   });
 });
