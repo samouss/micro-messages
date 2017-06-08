db0843d741163162717087c285c81968fb7fa9b6
Vaillant Samuel <samuel.vllnt@gmail.com>
2017-06-08 15:40:18 +0200

    fix: PORT env

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import path from 'path';
 
 const app = express();
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname));
 
