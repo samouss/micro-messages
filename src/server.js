4ee5b2885fcaf4fc7bbc7a629bb6090c4c0d6686
Vaillant Samuel <samuel.vllnt@gmail.com>
2017-06-08 14:58:41 +0200

    core: add server for run the app

---
+++ src/server.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import path from 'path';
+
+const app = express();
+const port = process.env.port || 3000;
+
+app.use(express.static(__dirname));
+
+app.get('*', (_, res) => {
+  res.sendfile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log('Server is running on', port);
+});
